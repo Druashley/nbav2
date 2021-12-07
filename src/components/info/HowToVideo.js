bf00722b89991885c041cbe5ce847857244b9075
Dru <druashley@gmail.com>
2021-12-06 17:23:57 -0800

    mobile fixes for the youtube video.

--- src/components/info/HowToVideo.js
+++ src/components/info/HowToVideo.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 export default function HowToVideo() {
+  const youtubeVideowidth = () => {
+    const { innerWidth: width } = window;
+    if (width > 1000) {
+      return 560;
+    } else {
+      return 300;
+    }
+  };
   return (
-    <div>
+    <div className="">
       <div className="text-center text-3xl pt-12">Video explanation </div>
       <iframe
-        width="560"
+        width={youtubeVideowidth()}
         height="315"
         src="https://www.youtube.com/embed/ELSVVdVIR34"
         title="YouTube video player"
