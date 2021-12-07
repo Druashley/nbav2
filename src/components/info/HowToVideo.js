b686e7b68273d948636d74ae522af9bcec75aa3b
Dru <druashley@gmail.com>
2021-12-06 17:28:13 -0800

    fixing console errors for the embeded youtube video.

--- src/components/info/HowToVideo.js
+++ src/components/info/HowToVideo.js
@@ -17,9 +17,9 @@ export default function HowToVideo() {
         height="315"
         src="https://www.youtube.com/embed/ELSVVdVIR34"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
